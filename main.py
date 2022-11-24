8d5cba1b205834a1d6501ca2657cb71419eb2bf9
Yaseen <fantasticyaseenshariff@gmail.com>
2022-11-24 18:58:26 +0530

    update v2

--- main.py
+++ main.py
@@ -1,55 +1,25 @@
 import random
+from steps import Game
 
-randomNumber = random.randint(2, 20)
-randomInt = 0
-if randomNumber % 2 == 0:
-    randomInt = randomNumber
+randomNumberEven = random.randint(2,20)
+randomNumber = 0
+if randomNumberEven % 2 == 0:
+    randomNumber = randomNumberEven
 
-inputStr = "\nINPUT (Hit Enter)"
+eg1 = 10 + randomNumber
+eg2 = 16 + randomNumber
 
-questions = [
-    "Guess a number between 1 to 10: ",
-    "Multiply with 2",
-    f"Add {randomInt} to it.",
-    "Divide it by 2",
-    "Subtract the number you guessed from the result",
-    f"Your answer would be: {round(randomInt / 2)}"
-]
+div_eg1 = round(eg1/2)
+div_eg2 = round(eg2/2)
+input_string = "INPUT (Hit Enter)"
 
-print("----------------------------------")
-# execute = methods_list[position](randomInt)
-# step_by.step_1()
-input(inputStr)
+funcs = Game(
+    eg1 = eg1,
+    eg2 = eg2,
+    div_eg1 = div_eg1,
+    div_eg2 = div_eg2,
+    input_string = input_string,
+    random_number = randomNumber
+)
 
-
-def __init__(self):
-    pass
-
-
-def step_1(self):
-    print("Ex: 5")
-
-
-def step_2(self):
-    print("Ex: if guess is 5 then 5x2=10")
-
-
-def step_3(self, randomInt):
-    eg1 = 10 + randomInt
-    print(f"Ex: 10 + {randomInt} = ", eg1)
-
-
-def step_4(self):
-    div_eg1 = round(eg1 / 2)
-    print(f"Ex: {eg1}/2 = {div_eg1}")
-
-
-def step_5(self):
-    print(f"Ex: if guess is 5 then subtract it {div_eg1}-5")
-
-
-def result(self):
-    print()
-
-
-methods_list = [step_1(), step_2(), step_3(), step_4(), step_5(), result()]
+funcs.run_game()
--- steps.py
+++ steps.py
@@ -1,3 +1,41 @@
 
+class Game:
+    def __init__(self, eg1, eg2, div_eg1, div_eg2, input_string, random_number):
+        self.eg1 = eg1
+        self.eg2 = eg2
+        self.div_eg1 = div_eg1
+        self.div_eg2 = div_eg2
+        self.input_string = input_string
+        self.randomNumber = random_number
 
+    def step1(self):
+        print("Guess a num b/w 1 to 10")
+        print("Ex: 5")
 
+    def step2(self):
+        print("Multiply with 2")
+        print("Ex: if guess is 5 then 5x2=10")
+
+    def step3(self):
+        print(f"Add {self.randomNumber} to it.")
+        print(f"Ex: 10 + {self.randomNumber} = ", self.eg1)
+
+    def step4(self):
+        print("Divide it by 2")
+        print(f"Ex: {self.eg1}/2 = {self.div_eg1}")
+
+    def step5(self):
+        print("Subtract the number you guess with the result")
+        print(f"Ex: if guess is 5 then subtract it {self.div_eg1}-5")
+
+    def result(self):
+        print(f"your answer would be: {round(self.randomNumber/2)}")
+
+    funcs = [step1, step2, step3, step4, step5]
+
+    def run_game(self):
+        for n in range(0, len(self.funcs)):
+            self.funcs[n](self)
+            input(self.input_string)
+            print("-------------------")
+        self.result()
